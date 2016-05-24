513fbc003e25b5226735621e77f0c3b5ce36a744
kengus <kengus@126.com>
2016-05-24 16:27:19 +0800

    add 8.9.10.11

---
+++ ex9.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+#Here's some new strange stuff remember type it exactly
+
+days = "Mon Tue Wen Thu Fir Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print "Here are the days:",days
+print "Here are the months:", months
+
+print """
+There's something going on here.
+with the three double_quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+"""
